4b005a0af462bdd44b57c2813b24125284e895bf
Ezz-Kalash <123479523+Ezz-Kalash@users.noreply.github.com>
2023-08-01 13:45:25 +0300

    update

---
+++ myscripts.js
@@ -0,0 +1,161 @@
+function dark() {
+
+
+    document.getElementById('dark').style.backgroundImage = 'url(Imges/image.jpg)'
+    document.getElementById('darkmode').style.display = 'none'
+    document.getElementById('notdark').style.display = 'inline'
+    document.getElementById('dd').style.color = 'white'
+    document.getElementById('skill').style.color = 'white'
+    document.getElementById('html').style.color = 'white'
+    document.getElementById('js').style.color = 'white'
+    document.getElementById('c').style.color = 'white'
+    document.getElementById('css').style.color = 'white'
+    document.getElementById('cont').style.color = 'white'
+    document.getElementById('project').style.color = 'white'
+    document.getElementById('progress').style.color = 'white'
+    document.getElementById('in').style.color = 'white'
+    document.getElementById('wats').style.color = 'white'
+    document.getElementById('face').style.color = 'white'
+    document.getElementById('insta').style.color = 'white'
+    document.getElementById('git').style.color = 'white'
+    document.getElementById('email').style.color = 'white'
+    document.getElementById('expe').style.color = 'white'
+    document.getElementById('experence_font_color').style.color = 'blue'
+    document.getElementById('git_img').style.backgroundColor = 'rgba(240, 248, 255, 0.617)'
+
+    document.getElementById('li').style.color = 'white'
+    document.getElementById('li2').style.color = 'white'
+    document.getElementById('li3').style.color = 'white'
+    document.getElementById('li4').style.color = 'white'
+    document.getElementById('li5').style.color = 'white'
+    document.getElementById('li6').style.color = 'white'
+    document.getElementById('thank').style.color = 'white'
+    document.getElementById('right').style.color = 'white'
+    document.getElementById('do_list_js').style.color = 'white'
+
+
+
+
+
+
+}
+function reset() {
+
+
+document.getElementById('dark').style.backgroundImage = 'url(Imges/bb.jpg)'
+document.getElementById('notdark').style.display = 'none'
+document.getElementById('darkmode').style.display = 'inline'
+
+document.getElementById('dd').style.color = 'black'
+    document.getElementById('skill').style.color = 'black'
+    document.getElementById('html').style.color = 'black'
+    document.getElementById('js').style.color = 'black'
+    document.getElementById('c').style.color = 'black'
+    document.getElementById('css').style.color = 'black'
+    document.getElementById('cont').style.color = 'black'
+    document.getElementById('project').style.color = 'black'
+    document.getElementById('progress').style.color = 'black'
+    document.getElementById('in').style.color = 'black'
+    document.getElementById('wats').style.color = 'black'
+    document.getElementById('face').style.color = 'black'
+    document.getElementById('insta').style.color = 'black'
+    document.getElementById('git').style.color = 'black'
+    document.getElementById('email').style.color = 'black'
+    document.getElementById('expe').style.color = 'black'
+    document.getElementById('git_img').style.backgroundColor = 'none'
+    document.getElementById('experence_font_color').style.color = 'black'
+    document.getElementById('li').style.color = 'black'
+    document.getElementById('li2').style.color = 'black'
+    document.getElementById('li3').style.color = 'black'
+    document.getElementById('li4').style.color = 'black'
+    document.getElementById('li5').style.color = 'black'
+    document.getElementById('li6').style.color = 'black'
+    document.getElementById('thank').style.color = 'black'
+    document.getElementById('right').style.color = 'black'
+    document.getElementById('do_list_js').style.color = 'black'
+
+
+
+
+}
+function front(){
+    document.getElementById('jsprojects').style.display = 'none'  
+    document.getElementById('progress').style.display = 'block'  
+    document.getElementById('soon_img').style.display = 'block'  
+    document.getElementById('jsp').style.backgroundColor = 'rgb(34, 0, 255)'  
+    document.getElementById('fep').style.backgroundColor = 'rgb(51, 51, 51)'  
+    document.getElementById('bep').style.backgroundColor = 'rgb(34, 0, 255)'  
+
+
+
+
+
+}
+function js(){
+    document.getElementById('jsprojects').style.display = 'flex'  
+    document.getElementById('progress').style.display = 'none'  
+    document.getElementById('soon_img').style.display = 'none'  
+    document.getElementById('jsp').style.backgroundColor = 'rgb(51, 51, 51)'  
+    document.getElementById('fep').style.backgroundColor = 'rgb(34, 0, 255)'  
+    document.getElementById('bep').style.backgroundColor = 'rgb(34, 0, 255)'  
+
+}
+function back(){
+    document.getElementById('jsprojects').style.display = 'none'  
+    document.getElementById('progress').style.display = 'block'  
+    document.getElementById('soon_img').style.display = 'block'  
+    document.getElementById('jsp').style.backgroundColor = 'rgb(34, 0, 255)'  
+    document.getElementById('fep').style.backgroundColor = 'rgb(34, 0, 255)'  
+    document.getElementById('bep').style.backgroundColor = 'rgb(51, 51, 51)'  
+
+
+}
+function img_hover(){
+    document.getElementById('gotogit').style.display = 'block'  
+    document.getElementById('do_list_img').style.filter = 'blur(2px)' 
+
+}
+function img_nothover(){
+    document.getElementById('gotogit').style.display = 'none'  
+    document.getElementById('do_list_img').style.filter = 'blur(0px)'  
+    document.getElementById('gotogit').style.color = 'white'  
+
+
+}
+function text_hover(){
+    document.getElementById('gotogit').style.display = 'block'  
+    document.getElementById('do_list_img').style.filter = 'blur(2px)' 
+    document.getElementById('gotogit').style.color = 'blue'  
+
+    
+}
+function text_nothover(){
+    document.getElementById('gotogit').style.display = 'none'  
+    document.getElementById('do_list_img').style.filter = 'blur(0px)' 
+    document.getElementById('gotogit').style.color = 'white'  
+
+}
+function img_hover_movie(){
+    document.getElementById('gotogitm').style.display = 'block'  
+    document.getElementById('movie_img').style.filter = 'blur(2px)' 
+
+}
+function img_nothover_movie(){
+    document.getElementById('gotogitm').style.display = 'none'  
+    document.getElementById('movie_img').style.filter = 'blur(0px)'  
+
+
+}
+function text_hover_movie(){
+    document.getElementById('gotogitm').style.display = 'block'  
+    document.getElementById('movie_img').style.filter = 'blur(2px)' 
+    document.getElementById('gotogitm').style.color = 'blue'  
+
+    
+}
+function text_nothover_movie(){
+    document.getElementById('gotogitm').style.display = 'none'  
+    document.getElementById('movie_img').style.filter = 'blur(0px)' 
+    document.getElementById('gotogitm').style.color = 'white'  
+
+}
